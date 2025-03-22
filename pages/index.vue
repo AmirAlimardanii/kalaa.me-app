7b65818e7d05d4d50612aaddc94581103f7a2809
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-22 21:47:39 +0430

    add admin page and implement logout functionality on home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,19 @@
 <template>
   <div>home page</div>
+  <Button @click="logout">Log Out</Button>
   <p>{{ user }}</p>
 </template>
 
 <script setup lang="ts">
-const { user } = useUserSession();
+definePageMeta({
+  middleware: "auth",
+});
+const { user, clear } = useUserSession();
+
+const logout = async () => {
+  await clear();
+  navigateTo("/auth/login");
+};
 </script>
 
 <style scoped></style>
