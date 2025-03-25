248b7161cc8e54fe5a2ddf690ca1fdfbcdc56eeb
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-25 17:46:21 +0430

    delete button functionality has been added

--- components/Admin/Category/Form.vue
+++ components/Admin/Category/Form.vue
@@ -3,30 +3,55 @@
     <div class="flex items-center justify-between">
       <Heading :title="title" :description="description" />
       <NuxtLink to="/admin/categories/new">
-        <Button v-if="isEditing" variant="destructive" size="lg">
-          Delete
+        <Button
+          v-if="isEditing"
+          variant="destructive"
+          size="sm"
+          class="mx-2 !h-10"
+          @Click="toggleAlertModal(true)"
+        >
+          <Icon name="lucide:trash" class="w-4 h-4" />
         </Button>
-        <Button v-else size="lg"> Add </Button>
+        <Button @click="onSubmit" :disabled="isLoading" size="lg">
+          {{ action }}
+        </Button>
       </NuxtLink>
     </div>
     <Separator class="my-4" />
 
-    <form></form>
+    <form class="space-y-8 w-full">
+      <div class="md:grid md:grid-cols-3 gap-8">
+        <FormField v-slot="{ componentField }" name="name">
+          <FormItem>
+            <FormLabel>Name</FormLabel>
+            <FormControl>
+              <Input placeholder="category name" v-bind="componentField" />
+            </FormControl>
+            <FormDescription />
+            <FormMessage />
+          </FormItem>
+        </FormField>
+      </div>
+    </form>
   </div>
+  <AlertModal @onConfirm="deleteCategory" title="Delete Category" />
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+
+const { isLoading, showError, showMessage, toggleLoading, toggleAlertModal } =
+  useStore();
+const route = useRoute();
 
 const title = ref("Edit Category");
 const description = ref("Edit category");
 const toastMessage = ref("Category Updated");
 const action = ref("Save changes");
-const isEditing = ref(true);
+const isEditing = ref(route.params.categoryId !== "new");
 
-const route = useRoute();
-
-const { data } = await useFetch(
+const { data: currentCategory } = await useFetch(
   `/api/admin/categories/${route.params.categoryId}`
 );
 
@@ -34,6 +59,47 @@ const formSchema = toTypedSchema(categorySchema);
 
 const form = useForm({
   validationSchema: formSchema,
-  initialValues,
+  initialValues: currentCategory.value || { name: "" },
 });
+
+const onSubmit = form.handleSubmit(async (values) => {
+  try {
+    toggleLoading(true);
+
+    if (isEditing.value) {
+      console.log("editing");
+    } else {
+      console.log("inserting");
+    }
+    showMessage({
+      title: toastMessage.value,
+      variant: "default",
+    });
+    navigateTo("/admin/categories");
+  } catch (error) {
+    const err = handleError(error);
+    showError(err);
+  } finally {
+    toggleLoading(false);
+  }
+});
+
+const deleteCategory = async () => {
+  try {
+    toggleLoading(true);
+
+    console.log("category deleted");
+
+    showMessage({
+      title: "Category Deleted",
+      variant: "default",
+    });
+    navigateTo("/admin/categories");
+  } catch (error) {
+    const err = handleError(error);
+    showError(err);
+  } finally {
+    toggleLoading(false);
+  }
+};
 </script>
