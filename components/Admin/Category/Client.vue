229fad652eaa5c4363316ce0c1e0256a1c341406
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-27 17:07:38 +0430

    get all categories

--- components/Admin/Category/Client.vue
+++ components/Admin/Category/Client.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="flex items-center justify-between">
-    <Heading title="Categories" description="Manage Categories"> </Heading>
-    <NuxtLink to="/admin/categories/new">
-      <Button>
-        <div>Add new +</div>
-      </Button>
-    </NuxtLink>
+  <div>
+    <div class="flex items-center justify-between">
+      <Heading title="Categories" description="Manage Categories"> </Heading>
+      <NuxtLink to="/admin/categories/new">
+        <Button>
+          <div>Add new +</div>
+        </Button>
+      </NuxtLink>
+    </div>
+    <div v-for="category in categories">
+      <div>{{ category.name }}</div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const { data: categories } = await useFetch("/api/admin/categories");
+</script>
--- server/api/admin/categories/index.get.ts
+++ server/api/admin/categories/index.get.ts
@@ -1,3 +1,10 @@
+import db from "~/utils/db";
 export default defineEventHandler(async (event) => {
-  return null;
+  const categories = await db.category.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return categories;
 });
