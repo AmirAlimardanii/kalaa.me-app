e8a27265483ef15c039419b91d47a1734288cf0b
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-28 02:21:21 +0430

    create data table

---
+++ components/Admin/Category/CellAction.vue
@@ -0,0 +1,18 @@
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="ghost" class="h-8 w-8 p-0">
+        <span class="sr-only">Open menu</span>
+        <Icon name="lucide:more-horizontal" class="w-4 h-4" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuItem>
+        <Icon name="lucide:copy" class="mr-2 h-4 w-4" />
+        <span class="mb-1">copy ID</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<script setup lang="ts"></script>
