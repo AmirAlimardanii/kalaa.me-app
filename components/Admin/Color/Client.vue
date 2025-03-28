b3a18365a9763ddc6ea12807dad835078fa2a6cb
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-28 22:07:09 +0430

    create colors page and table

---
+++ components/Admin/Color/Client.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="flex items-center justify-between">
+      <Heading title="Colors" description="Manage Colors"> </Heading>
+      <NuxtLink to="/admin/colors/new">
+        <Button>
+          <div>Add new +</div>
+        </Button>
+      </NuxtLink>
+    </div>
+    <DataTable
+      class="mt-4"
+      v-if="status !== 'pending'"
+      column-to-search="name"
+      :data="colors"
+      :columns="columns"
+    />
+  </div>
+</template>
+
+<script setup>
+import { columns } from "~/components/Admin/Category/column";
+const { data: colors, status } = await useFetch("/api/admin/colors", {
+  key: "categories",
+});
+</script>
