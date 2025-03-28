591b21baf9a90104593ffc345161ad0624907993
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-28 22:15:38 +0430

    post and get color api created

--- utils/validation.ts
+++ utils/validation.ts
@@ -2,14 +2,28 @@ import { string, z } from "zod";
 
 export const authSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(4, "Password must be at least 4 characters long"),
+  password: z
+    .string()
+    .min(4, { message: "Password must be at least 4 characters long" }),
   name: z
     .string()
-    .min(2, "Name must be at least 2 characters long")
+    .min(2, { message: "Name must be at least 2 characters long" })
     .default("")
     .optional(),
 });
 
 export const categorySchema = z.object({
-  name: z.string().min(2, "Category name must be at least 2 characters long"),
+  name: z
+    .string()
+    .min(2, { message: "Category name must be at least 2 characters long" }),
+});
+
+export const colorSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "color name must be at least 2 characters long" }),
+  value: z
+    .string()
+    .min(1, { message: "color value must be at least 2 characters long" })
+    .default("#000000"),
 });
