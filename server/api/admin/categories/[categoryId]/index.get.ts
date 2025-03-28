6ba495f67126b7205d3a0c2148b69d4ee54765fc
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-28 16:17:35 +0430

    category details api route

--- server/api/admin/categories/[categoryId]/index.get.ts
+++ server/api/admin/categories/[categoryId]/index.get.ts
@@ -1,3 +1,9 @@
+import db from "~/utils/db";
 export default defineEventHandler(async (event) => {
-  return null;
+  const category = await db.category.findUnique({
+    where: {
+      id: event.context.params?.categoryId,
+    },
+  });
+  return category;
 });
