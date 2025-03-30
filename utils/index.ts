61f86a3254a93354b37086584f1396c62b0a4d01
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-30 19:45:32 +0430

    currency formatter

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,8 @@
 export const imageResourceName = (url: string) => {
   return url.split(".")[url.split(".").length - 2].split("/").pop();
 };
+
+export const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
