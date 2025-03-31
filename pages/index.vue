e04de75ed471d5e2f2bcb80a5a7f5e13df7f46a3
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-31 14:22:29 +0430

    create hero part

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,9 @@
 <template>
-  <div>home page</div>
-  <Button @click="logout">Log Out</Button>
-  <p>{{ user }}</p>
+  <Hero />
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   middleware: "auth",
 });
-const { user, clear } = useUserSession();
-
-const logout = async () => {
-  await clear();
-  navigateTo("/auth/login");
-};
 </script>
-
-<style scoped></style>
