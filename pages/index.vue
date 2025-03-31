93ad8d3685836a8dba99bb170c13aba2717e24ce
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-03-31 14:25:56 +0430

    create product container

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
   <Hero />
+  <div class="mx-auto max-w-5xl py-16 sm:py-32">
+    <div class="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8"></div>
+  </div>
 </template>
 
 <script setup lang="ts">
