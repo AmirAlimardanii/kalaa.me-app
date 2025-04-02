c5179fc42b33a9657a505b8e62f6cf0b42816b01
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-04-02 20:01:21 +0430

    add checkout feature

--- server/api/checkout/index.post.ts
+++ server/api/checkout/index.post.ts
@@ -1,3 +1,4 @@
+import { stripe } from "~/server/utils/stripe";
 import db from "~/utils/db";
 
 export default defineEventHandler(async (event) => {
@@ -33,5 +34,30 @@ export default defineEventHandler(async (event) => {
     },
   });
 
-  return order;
+  const session = await stripe.checkout.sessions.create({
+    line_items: products.map((product) => {
+      return {
+        quantity: 1,
+        price_data: {
+          currency: "USD",
+          product_data: {
+            name: product.name,
+          },
+          unit_amount: product.price * 100,
+        },
+      };
+    }),
+    mode: "payment",
+    billing_address_collection: "required",
+    phone_number_collection: {
+      enabled: true,
+    },
+    success_url: `http://localhost:3000/cart?success=1`,
+    cancel_url: `http://localhost:3000/cart?canceled=1`,
+    metadata: {
+      orderId: order.id,
+    },
+  });
+
+  return session.url;
 });
