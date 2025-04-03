5fab080cda2b0ea6dd28dbaba419193b0e95c2fa
Amir Alimadani <amiralimardani.dev@gmail.com>
2025-04-03 12:19:29 +0430

    Prepare  project

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  compatibilityDate: "2024-04-03",
+  nitro: {
+    preset: "node-server",
+  },
+  devtools: { enabled: false },
   modules: [
-    '@nuxtjs/tailwindcss',
-    'shadcn-nuxt',
-    '@vueuse/nuxt',
-    '@nuxt/icon',
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@vueuse/nuxt",
+    "@nuxt/icon",
     "@nuxtjs/cloudinary",
-    'nuxt-auth-utils',
+    "nuxt-auth-utils",
   ],
   shadcn: {
-    prefix: '',
-    componentDir: './components/ui'
+    prefix: "",
+    componentDir: "./components/ui",
   },
   runtimeConfig: {
-    githubId: '',
-    githubSecret: '',
-    stripeSecret: '',
-    stripeWebhookSecret: '',
+    githubId: "",
+    githubSecret: "",
+    stripeSecret: "",
+    stripeWebhookSecret: "",
     public: {
-      cloudinaryCloudName: '',
-      uploadPreset: '',
-      stripeKey: '',
+      cloudinaryCloudName: "",
+      uploadPreset: "",
+      stripeKey: "",
     },
-    cloudinaryApiKey: '',
-    cloudinaryApiSecret: '',
+    cloudinaryApiKey: "",
+    cloudinaryApiSecret: "",
   },
   cloudinary: {
     cloudName: process.env.NUXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
     uploadPreset: process.env.NUXT_PUBLIC_UPLOAD_PRESET,
     apiKey: process.env.NUXT_CLOUDINARY_API_KEY,
   },
-})
+});
